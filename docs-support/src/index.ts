d46d1ece8b612c73ad6cd9b46668bc0511f88ec0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-09 19:31:31 -0500

    Add theme-toggle and callout

--- docs-support/src/index.ts
+++ docs-support/src/index.ts
@@ -1,4 +1,5 @@
 export { H2 } from './h2.gts';
+export { Callout } from './callout.gts';
 export { Text } from './text.gts';
 export { Article } from './article.gts';
 export { Shell } from './shell.gts';
@@ -6,3 +7,4 @@ export { IndexPage } from './index-page.gts';
 export { Link, InternalLink } from './links.gts';
 export { OopsError } from './errors.gts';
 export { Layout, PageLayout } from './layouts.gts';
+export { ThemeToggle } from './theme-toggle.gts';
